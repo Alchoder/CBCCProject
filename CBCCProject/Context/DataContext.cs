2dea5027c9e29d3134589d8f218e8b696596d26d
hamza marais <hamza.gmail@gmail.com>
2019-05-19 23:50:03 +0200

    Valider

---
+++ CBCCProject/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using CBCCProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CBCCProject.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<CBCCProject.Entities.Job> Job { get; set; }
+    }
+
+
+}
